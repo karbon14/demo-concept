4376ad98878d4d93d4fb9d1705925c2d67289df6
Vision <41881618+visionk14@users.noreply.github.com>
2018-12-19 17:31:13 -0300

    Changes in order to work over RSK (#28)

--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
-const { ROPSTEN_MNEMONIC, INFURA_API_KEY } = process.env
+const { ROPSTEN_MNEMONIC, MAINNET_MNEMONIC, RSK_TESTNET_MNEMONIC, INFURA_API_KEY } = process.env
 
 module.exports = {
   migrations_directory: './migrations',
@@ -15,13 +15,19 @@ module.exports = {
       network_id: '3'
     },
     live: {
-      provider: () => new HDWalletProvider(ROPSTEN_MNEMONIC, `https://mainnet.infura.io/v3/${INFURA_API_KEY}`),
+      provider: () => new HDWalletProvider(MAINNET_MNEMONIC, `https://mainnet.infura.io/v3/${INFURA_API_KEY}`),
       gas: 4698712,
       network_id: '1'
     },
     rpc: {
       host: '127.0.0.1',
       port: 8545
+    },
+    RSKtestnet: {
+      provider: () => new HDWalletProvider(RSK_TESTNET_MNEMONIC, 'https://public-node.testnet.rsk.co:443'),
+      network_id: '*',
+      gas: 2500000,
+      gasPrice: 183000
     },
     solc: {
       optimizer: {
